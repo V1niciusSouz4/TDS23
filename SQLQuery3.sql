14a6ee08948b10d6fe741940298fe9cdd8a5f387
VINÍCIUS SOUZA SANTOS <3343.343@edu.fb.org.br>
2023-05-25 21:52:47 -0300

    Banco de dados

---
+++ SQLQuery3.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE auladoin
+
+USE auladoin
+
+CREATE TABLE Tabelas
+INSERT INTO TabelaB (IdNome, nome)
+
+VALUES (1, 'Espada')
+
+INSERT INTO TabelaB (IdNome, nome)
+VALUES (2, 'Robo')
+
+INSERT INTO TabelaB (IdNome, nome)
+VALUES (3, 'Mario')
+
+INSERT INTO TabelaB (IdNome, nome)
+VALUES (4, 'Samurai')
+
+
+SELECT * FROM TabelaB
+
+SELECT * FROM TabelaA
+INNER JOIN TabelaB
+ON TabelaA.nome = TabelaB.nome
